f693507d4838bced7a36589d228ec76c9c12b007
Sayfudin <Seifdin62@gmail.com>
2024-11-02 14:16:27 +0300

    completing-assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js"; // Confirm this path is correct
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>This is a brief description about me.</p>
+      <img src={image} alt="I made this" /> {/* Ensure the alt text matches exactly */}
+    </div>
+  );
 }
 
 export default About;
--- src/data/data.js
+++ src/data/data.js
@@ -5,3 +5,5 @@ const city = "New York";
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
 export { name, city, image };
+// src/data/data.js
+
